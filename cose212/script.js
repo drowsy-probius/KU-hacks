24b7cfc6f22f33d4b76881cb354b35eb689ba595
k123s456h <k123s456h@gmail.com>
2022-09-16 16:01:33 +0900

    Update scripts

--- cose212/script.js
+++ cose212/script.js
@@ -9,7 +9,7 @@
 (function () {
   'use strict';
 
-  window.autoSaverLastTime = 0;
+  window.autoSaverTimer = -1;
   window.autoSaverSleepMs = 500;
   const autoSaver = async () => {
     const isOutOfDue = (dueString) => {
@@ -23,59 +23,59 @@
     const problem_selected = vm.problem_selected;
     if (problem_selected === -1 || hw_selected === -1) return;
     if (!("name" in problemSet[problem_selected]) || !("term" in problemSet[problem_selected])) return;
-    const Problemname = problemSet[problem_selected].name;
+    const problem_name = problemSet[problem_selected].name;
     const due_date = problemSet[problem_selected].term;
     if (isOutOfDue(due_date)) return;
     const editorValue = editor.getValue();
+    if(editorValue.trim().length === 0) return;
 
-    if (Date.now() - window.autoSaverLastTime < window.autoSaverSleepMs)
-      return console.log(`[AUTOSAVER] sleep for a while due to too many requests.`);
-
-    const loginRes = await fetch(`/php/validate-login.php`, {
-      method: "POST",
-      cache: "no-cache"
-    });
-    if (!loginRes.ok) return alert_login();
-    const userName = await loginRes.text();
-    if (userName.length === 0) return alert_login();
-
-    const params = {
-      problem: Problemname,
-      contents: editorValue,
-      due: due_date,
-    }
-    const formBody = Object.keys(params).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`).join("&");
-
-    const saveRes = await fetch(`/php/save.php`, {
-      method: "POST",
-      cache: "no-cache",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: formBody,
-    });
-    window.autoSaverLastTime = Date.now();
-
-    if (!saveRes.ok) {
-      return swal("Submit Error", JSON.stringify(await saveRes.text()), "error");
-    }
-    const saveResData = await saveRes.text();
-    switch (saveResData) {
-      case "success":
-        return;
-      case "abuse":
-        await swal("Catch system library", "The program includes system call. Check your code and delete the usage of system modules", "error")
-        return editor.focus();
-      case "length":
-        await swal("Limited length", "Your code is too large. please reduce it", "error");
-        return editor.focus();
-      case "over":
-        await swal("Over due date", "Since the due date is over, submit function is closed.", "error");
-        return editor.focus();
-      default:
-        await swal("Fail to save", "Try again to submit your code. If the error is continuously occured, please contact us.", "error");
-        return editor.focus();
-    }
+    clearInterval(window.autoSaverTimer);
+    window.autoSaverTimer = setTimeout(async () => {
+      const loginRes = await fetch(`/php/validate-login.php`, {
+        method: "POST",
+        cache: "no-cache"
+      });
+      if (!loginRes.ok) return alert_login();
+      const userName = await loginRes.text();
+      if (userName.length === 0) return alert_login();
+  
+      const params = {
+        problem: problem_name,
+        contents: editorValue,
+        due: due_date,
+      }
+      const formBody = Object.keys(params).map(k => `${encodeURIComponent(k)}=${encodeURIComponent(params[k])}`).join("&");
+  
+      const saveRes = await fetch(`/php/save.php`, {
+        method: "POST",
+        cache: "no-cache",
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+        body: formBody,
+      });
+  
+      if (!saveRes.ok) {
+        return swal("Submit Error", JSON.stringify(await saveRes.text()), "error");
+      }
+      const saveResData = await saveRes.text();
+      switch (saveResData) {
+        case "success":
+          return;
+        case "abuse":
+          await swal("Catch system library", "The program includes system call. Check your code and delete the usage of system modules", "error")
+          return editor.focus();
+        case "length":
+          await swal("Limited length", "Your code is too large. please reduce it", "error");
+          return editor.focus();
+        case "over":
+          await swal("Over due date", "Since the due date is over, submit function is closed.", "error");
+          return editor.focus();
+        default:
+          await swal("Fail to save", "Try again to submit your code. If the error is continuously occured, please contact us.", "error");
+          return editor.focus();
+      }
+    }, window.autoSaverSleepMs);
   }
 
   // editor.on("input", autoSaver)
